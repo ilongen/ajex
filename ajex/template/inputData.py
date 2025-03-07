c8d4b826d1bb424b1bd37f4747aa5cb3c405ea05
devlongen <iago.mendonca_2005@hotmail.com>
2025-03-06 18:07:56 -0600

    work: from class inputData creating method option/convert/validation.

--- ajex/template/inputData.py
+++ ajex/template/inputData.py
@@ -1,26 +1,26 @@
 from libs import *
+from django.http import request
+from django.http import HttpResponse
+
 class inputData():
-    sheet = pd.DataFrame()
-    date:time
-    optionUser:str
-
     def validationInput(sheet:pd.DataFrame):
-        print("Test")
-
+        if type(sheet) == ".xlsx" or type(sheet) == ".csv":
+            return sheet
+        else:
+            return HttpResponse("File not supported, try again!")
     def convertInput(sheet:pd.DataFrame):
         print("Create options .csv or .xlsx")
+    def optionUserSelected(optionUser,sheetValidated):
+        usingFunction=inputData
+        if optionUser is not None:
+            if optionUser is ".csv":
+                sheetNew = usingFunction.convertInput(sheetValidated)
+            elif optionUser is ".xlsx":
+                sheetNew = usingFunction.convertInput(sheetValidated)
+                return sheetNew    
 
-    #sheet = requests.POST.get("formPost")
+    sheet = request.POST.get("formPost")
     date:time
     optionUser:str
-
-
-    def validationInput(sheet:pd.DataFrame):
-        print("Test")
-        return sheet
-
-    def convertInput(sheet:pd.DataFrame):
-        print("Create options .csv or .xlsx")
-        return sheet
-    sheet = validationInput(sheet)
-    sheetNew = convertInput(sheet)
+    sheetValidated = validationInput(sheet)
+    nextStep = optionUserSelected(optionUser,sheetValidated)
