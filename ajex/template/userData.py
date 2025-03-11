53a05b78ab3901c9e51cbcb069c989068444a945
Iago Longen <125667107+devlongen@users.noreply.github.com>
2025-03-11 19:33:53 -0300

    test: need to test The code on the server

--- ajex/template/userData.py
+++ ajex/template/userData.py
@@ -3,9 +3,9 @@ import pandas as pd
 class userData():
 
     def optionUserSelected(args,optionUser):
-        if optionUser == ".csv":
+        if optionUser in args:
             sheetNew = pd.read_csv(args)
             return sheetNew
-        elif optionUser == ".xlsx":
+        elif optionUser in args:
             sheetNew = pd.read_excel(args)
             return sheetNew    
