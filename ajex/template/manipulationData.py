bff9a166ab9ed9751f094fdbd92230a97e88aaff
devlongen <iago.mendonca_2005@hotmail.com>
2025-04-05 00:03:59 -0300

    new: class and method are almost 55% complete, I'm still working on it.

--- ajex/template/manipulationData.py
+++ ajex/template/manipulationData.py
@@ -2,18 +2,20 @@ import pandas as pd
 class manipulationData():
     def deletRow_isNull(param):
         n_row, n_columns = param.shape()
-        dictList=[]
-        porcColumn = n_columns*0.60 # %            
+        dictNA={}
+        porcColumn_min = n_columns*0.60 # %
+        countColumn_NA=0            
         for i in range(n_row):
             for j in range(n_columns):
                 value=param.iloc[i,j]
-                if pd.isna(value)==True:
-                    dictData={
-                        "row": i,
-                        "column": j,
-                        "value":value
-                    }
-                    dictList.append(dictData)
-                    for z in range(len(dictList)):
-                        print(dictList)
-                        # Working for now on the part
+                try:
+                    if pd.isna(value)==True:
+                        dictNA={"row":i,"column":j,"value":value}
+                        if dictNA['row'] == i and dictNA["column"] != 0:
+                            countColumn_NA+=1
+                        else:
+                            return "Error server count(column)"
+                    else:
+                        return None
+                except:
+                    print("Value Not  NA")        
--- ajex/tests.py
+++ ajex/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
-# Create your tests here.
+import pandas as pd
+
+
+sheet = pd.read_excel(r'/home/iagolongen/Documents/39.268.3960001-98/PLANILHA_ESTOQUE_COMPLETO_LJ.xlsx')
+n_row, n_columns = sheet.shape
+dictNA={}
+porcColumn_min = n_columns*0.60 # %            
+for i in range(n_row):
+    for j in range(n_columns):
+        value=sheet.iloc[i,j]
+        if pd.isna(value)==True:
+           dictNA={"row":i,"column":j,"value":value}
+           if dictNA['row'] == i:
+                print(dictNA)
