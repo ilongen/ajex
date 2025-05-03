7b5006bbf5113cccfac0317027389ff4d72a1f3b
devlongen <iago.mendonca_2005@hotmail.com>
2025-05-03 16:58:25 -0300

    feat: new functionality, refactored, doing documentation now.
    message: This functionality was redropped and worked on the factors that were needed and is now having a new particularity, it has two types of returns of spreadsheets one with 100% correct data and another with exceptions to be worked, user will be able to work as desired.

--- ajex/views.py
+++ ajex/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from api.user_data.userData import userData
 from api.services.manipulationData import manipulationData
 import io
-from django.http import FileResponse
+from django.http import StreamingHttpResponse
+import zipfile
 # Create your views here.
 
 def index(request):
@@ -25,8 +26,29 @@ def user_data(request):
 def manipulation_data_local(param):
         sheetManipulation = manipulationData(df=param)
         sheetManipulation.valueCell_isna()
+        dfException = sheetManipulation.dataframe_expection()
         dfOutput = sheetManipulation.deletCell()
-        buffer = io.BytesIO()
-        dfOutput.to_excel(excel_writer=buffer,index=True)
-        buffer.seek(0)
-        return FileResponse(buffer,as_attachment=True,filename=f"data.xlsx",content_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+        zip_buffer = io.BytesIO()
+
+        with zipfile.ZipFile(zip_buffer, mode="w", compression=zipfile.ZIP_DEFLATED) as zf:
+            buffer_output = io.BytesIO()
+            buffer_except = io.BytesIO()
+
+            dfOutput.to_excel(excel_writer=buffer_output, index=True)
+            dfException.to_excel(excel_writer=buffer_except, index=True)
+
+            buffer_output.seek(0)
+            buffer_except.seek(0)
+
+            zf.writestr("data_output.xlsx", buffer_output.read())
+            zf.writestr("data_exception.xlsx", buffer_except.read())
+
+        zip_buffer.seek(0)
+
+        response = StreamingHttpResponse(
+                zip_buffer,
+        content_type="application/zip"
+        )
+        response['Content-Disposition'] = 'attachment; filename="data_compact.zip"'
+
+        return response
