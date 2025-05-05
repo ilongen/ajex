9d3d9149ac6c1f0acea354f5d008fc82dd267160
devlongen <iago.mendonca_2005@hotmail.com>
2025-05-05 13:32:15 -0300

    hotfix: remove files

--- ajex/settings.py
+++ ajex/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9if-5lzhp5m$7nxxj9otw+nq6exi&hv$j7wrf)0whju^8m6^@i'
+SECRET_KEY = os.environ("config.env")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
