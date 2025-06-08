bd17653275514a036a6b57de74f28490b20ee5a7
devlongen <iago.mendonca_2005@hotmail.com>
2025-06-08 15:53:38 -0300

    HOTFIX: CHANGE

--- project/settings.py
+++ project/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
+        'DATABASE_NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
         'HOST': config('DB_HOST'),
